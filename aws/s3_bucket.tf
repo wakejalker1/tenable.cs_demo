bace0edbf9320222df43fa0364b4fbec009f9162
Accurics <admin@accurics.com>
2022-04-15 11:56:41 -0700

    Auto Remediation By Accurics

--- aws/s3_bucket.tf
+++ aws/s3_bucket.tf
@@ -2,4 +2,8 @@ resource "aws_s3_bucket" "tenable_cs_demo_s3_bucket" {
   bucket = "tenablecsdemos3bucket"
   acl    = "public-read"
   tags   = var.default_tags
+
+  versioning {
+    enabled = true
+  }
 }
