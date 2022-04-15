eea340bef4ad64d2c7ce140b013f0fb52bc8f765
Accurics <admin@accurics.com>
2022-04-15 07:43:45 -0700

    Auto Remediation By Accurics

--- aws/s3_bucket.tf
+++ aws/s3_bucket.tf
@@ -2,4 +2,12 @@ resource "aws_s3_bucket" "tenable_cs_demo_s3_bucket" {
   bucket = "tenablecsdemos3bucket"
   acl    = "public-read"
   tags   = var.default_tags
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
